469a844b5729f6ad2c0e9445790b0f77cfeffc60
root <root@hkdfhgf56954998726399908135.local>
2026-03-24 16:24:58 +0000

    更新版本号至 2.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mosetian.passwordmanager"
         minSdk = 28
         targetSdk = 35
-        versionCode = 19
-        versionName = "2.3.1"
+        versionCode = 20
+        versionName = "2.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
